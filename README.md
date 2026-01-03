e5eb91ff97078a6de6b881175ea1c6f62f6d868c
coded-with-claws <79459323+coded-with-claws@users.noreply.github.com>
2026-01-03 23:09:33 +0100

    handle db, leech youtube links

---
+++ README.md
@@ -0,0 +1,19 @@
+# feedleech
+
+Auto-leech links from feeds.
+Only ATOM 1.0 feeds supported.
+
+Feeds URL must be filled into `config.toml` configuration file.
+
+A `feedleech_db.toml` file will be created by the script to memorize the last items it downloaded.
+
+## Pre-requisite
+
+- python3
+- dependencies installed from requirements.txt
+
+## Tests
+
+You can do some tests hosting a feed (like the ones in `examples` directory) with `python3 -m http.server 8080`.
+Configure URL `http://127.0.0.1:8080/feed.atom` and switch the feed data with `ln -sf feednnn.atom feed.atom`.
+
---
+++ feedleech.py
@@ -0,0 +1,195 @@
+# vim: set syntax=python:
+
+#TODO
+# - lib newspaper3k to leech articles (https://pypi.org/project/newspaper3k/)
+
+import feedparser
+import tomllib
+import tomli_w
+import os
+import pickle
+import yt_dlp
+
+CONFIG_FILE_NAME = "config.toml"
+DB_FILE_NAME = "feedleech_db.toml"
+LEECH_DIR = "leech"
+ATTR_LAST_LEECH = "last_leech"
+
+def main():
+    db_data = {}
+    feed_data = {}
+    print("feedleech initializing...")
+    os.makedirs(LEECH_DIR, exist_ok=True)
+    # load config
+    conf = config_load()
+    feeds_urls = conf["feeds"]["feeds_url"]
+    if not feeds_urls:
+        print(f"no URLs found in configuration file {CONFIG_FILE_NAME}")
+    print(f"feed URLs: {feeds_urls}")
+    # create / open db
+    db_data, is_db_created = db_create_load()
+    if not is_db_created:
+        print(f"db loaded: {db_data.keys()}")
+        #init_last_entry(db_data)
+        for k in db_data:
+            last_entry = db_data[k][ATTR_LAST_LEECH]
+            print(f"{k}: last entry leeched = {last_entry}")
+        #print(f"{db_data}")
+    else:
+        db_data = {}
+    # inject configured urls into db (if not already there)
+    init_feedurls_db(feeds_urls, db_data)
+    # get feeds
+    get_feeds(feeds_urls, feed_data)
+    # leech new items
+    leech_res = leech_new_entries(feed_data, db_data)
+    if not leech_res:
+        # do not update db
+        return -1
+    # update db
+    db_update(db_data)
+
+def config_load():
+    conf_handle = None
+    with open(CONFIG_FILE_NAME, "rb") as f:
+        conf_handle = tomllib.load(f)
+    return conf_handle
+
+def db_create_load():
+    db_content = None
+    db_created = False
+    try:
+        db_handle = open(DB_FILE_NAME, "rb")
+        db_content = tomllib.load(db_handle)
+        db_handle.close()
+        print("db found, fetched data")
+    except(FileNotFoundError):
+        empty_data = {}
+        db_handle = open(DB_FILE_NAME, "wb")
+        tomli_w.dump(empty_data, db_handle)
+        db_handle.close()
+        db_created = True
+        print("db not found, created")
+    return db_content, db_created
+
+def get_feeds(urls: list, db_data):
+    for u in urls:
+        print(f"getting content of {u}")
+        content = get_feed(u)
+        if content:
+            db_data[u] = content
+
+def get_feed(url: str):
+    feed_content = feedparser.parse(url)
+    if feed_content["status"] == 200:
+        return feed_content
+    else:
+        return None
+
+#def init_last_entry(db_data):
+#    for k in db_data:
+#        try:
+#            last_entry = db_data[k][ATTR_LAST_LEECH]
+#        except(KeyError):
+#            db_data[k][ATTR_LAST_LEECH] = None
+
+def init_feedurls_db(urls, db_data):
+    for u in urls:
+        try:
+            db_data[u]
+        except(KeyError):
+            db_data[u] = {}
+            db_data[u][ATTR_LAST_LEECH] = None
+
+# leech new entries and update last leech
+def leech_new_entries(feed_data, db_data):
+    leech_new_entries_res = True
+    is_new_entries = False
+    for u in db_data:
+        last_leech_db = db_data[u][ATTR_LAST_LEECH]
+        last_leech_feed = feed_data[u].entries[0]["id"]
+        print(f"feed {u}, comparing db last leech {last_leech_db} with feed last leech {last_leech_feed}")
+        if last_leech_db != last_leech_feed:
+            print("new shit found into feed")
+            is_new_entries = True
+            new_entries = get_new_entries(feed_data, u, last_leech_db)
+            # leech new entries
+            for n in new_entries:
+                leech_res = leech_entry(u, n)
+                if leech_res:
+                    update_last_leech(feed_data, db_data, u, n)
+                else:
+                    print(f"FAILED leeching {n['link']}")
+                    leech_new_entries_res = False
+                    break
+        else:
+            print("nothing new found into feed")
+    return leech_new_entries_res
+
+def get_new_entries(feed_data, feed_url, last_leech_db):
+    # seek last leech entry in new feed
+    # if not found, all entries must be leeched
+    entries_to_leech = []
+    for e in feed_data[feed_url].entries:
+        if e["id"] == last_leech_db:
+            #print(f"last leech found: {e}")
+            print(f"last leech found into new feed {last_leech_db}")
+            break
+        else:
+            entries_to_leech.append(e)
+    return entries_to_leech
+
+def leech_entry(url, entry):
+    leech_res = False
+    link = entry["link"]
+    print(f"leeching {entry['title']} {link}")
+    if ("youtube.com" in link or
+        "youtu.be" in link):
+        leech_res = leech_entry_yt(link)
+    else:
+        print(f"no extractor found for {link}")
+    return leech_res
+
+def leech_entry_yt(url):
+    yt_dlp_res = True
+    yt_paths = {}
+    yt_paths["home"] = LEECH_DIR
+
+    ydl_opts = {
+        'paths': yt_paths,
+        'format': 'm4a/bestaudio/best',
+        # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+        }]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            error_code = ydl.download(url)
+            #print(f"yt_dlp error_code {error_code}")
+            if error_code != 0:
+                yt_dlp_res = False
+        except yt_dlp.utils.YoutubeDLError as e:
+            #print(f"yt_dlp error {e}")
+            yt_dlp_res = False
+        except(Exception):
+            yt_dlp_res = False
+    return yt_dlp_res
+
+def update_last_leech(feed_data, db_data, url, entry):
+    entry_id = entry["id"]
+    print(f"last entry for {url}: {entry_id}")
+    db_data[url][ATTR_LAST_LEECH] = entry_id
+
+def db_update(db_data):
+    #print(f"data to dump into db:\n{db_data}")
+    with open(DB_FILE_NAME, "wb") as f:
+        tomli_w.dump(db_data, f)
+    f.close()
+    print("db saved")
+
+if __name__ == "__main__":
+    main()
+
