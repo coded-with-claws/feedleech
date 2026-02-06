61e29252755db91ba1bdccd566291ebb41af3139
coded-with-claws <79459323+coded-with-claws@users.noreply.github.com>
2026-02-06 22:48:46 +0100

    style: format logs

--- feedleech.py
+++ feedleech.py
@@ -30,12 +30,12 @@ def main():
     argparser.add_argument("config_file", help="config file in TOML format")
     args = argparser.parse_args()
     config_file = args.config_file
-    print(f"config file: {config_file}")
+    print(f"[+] config file: {config_file}")
 
     # initialize
     db_data = {}
     feed_data = {}
-    print("feedleech initializing...")
+    print("[*] feedleech initializing...")
 
     # load config
     conf = config_load(config_file)
@@ -46,17 +46,17 @@ def main():
         LEECH_DIR = conf["general"]["leech_dir"]
         feeds_urls = conf["feeds"]["feeds_url"]
     except KeyError as e:
-        print(f"exit: missing parameters into configuration file")
+        print(f"[!] exit: missing parameters into configuration file")
         return -1
     if not LEECH_DIR:
-        print(f"exit: no leech directory found in configuration file")
+        print(f"[!] exit: no leech directory found in configuration file")
         return -1
     if not feeds_urls:
-        print(f"exit: no URLs found in configuration file {config_file}")
+        print(f"[!] exit: no URLs found in configuration file {config_file}")
         return -1
     DB_FILE_NAME = str(config_file).replace(".toml", ".db")
-    print(f"leech directory: {LEECH_DIR}")
-    print(f"feed URLs: {feeds_urls}")
+    print(f"[+] leech directory: {LEECH_DIR}")
+    print(f"[+] feed URLs: {feeds_urls}")
 
     # create leech directory (if doesn't already exists)
     os.makedirs(LEECH_DIR, exist_ok=True)
@@ -64,11 +64,11 @@ def main():
     # create / open db
     db_data, is_db_created = db_create_load()
     if not is_db_created:
-        print(f"db loaded: {db_data.keys()}")
+        print(f"[+] db loaded: {db_data.keys()}")
         #init_last_entry(db_data)
         for k in db_data:
             last_entry = db_data[k][ATTR_LAST_LEECH]
-            print(f"{k}: last entry leeched = {last_entry}")
+            print(f"  {k}: last entry leeched = {last_entry}")
         #print(f"{db_data}")
     else:
         db_data = {}
@@ -77,7 +77,7 @@ def main():
     init_feedurls_db(feeds_urls, db_data)
 
     # get feeds
-    print("getting feeds...")
+    print("[*] getting feeds...")
     get_feeds(feeds_urls, feed_data)
 
     # leech new items
@@ -94,13 +94,13 @@ def main():
 def config_load(config_filename):
     conf_handle = None
     if not str(config_filename).lower().endswith(".toml"):
-        print(f"{config_filename} doesn't end with .toml extension")
+        print(f"[!] {config_filename} doesn't end with .toml extension")
         return conf_handle
     with open(config_filename, "rb") as f:
         try:
             conf_handle = tomllib.load(f)
         except tomllib.TOMLDecodeError as e:
-            print(f"{config_filename} doesn't seem to be in TOML format")
+            print(f"[!] {config_filename} doesn't seem to be in TOML format")
     return conf_handle
 
 def db_create_load():
@@ -110,19 +110,19 @@ def db_create_load():
         db_handle = open(DB_FILE_NAME, "rb")
         db_content = tomllib.load(db_handle)
         db_handle.close()
-        print("db found, fetched data")
+        print("[+] db found, fetched data")
     except(FileNotFoundError):
         empty_data = {}
         db_handle = open(DB_FILE_NAME, "wb")
         tomli_w.dump(empty_data, db_handle)
         db_handle.close()
         db_created = True
-        print("db not found, created")
+        print("[+] db not found, created")
     return db_content, db_created
 
 def get_feeds(urls: list, db_data):
     for u in urls:
-        print(f"getting content of {u}")
+        print(f"[*] getting content of {u}")
         content = get_feed(u)
         if content:
             db_data[u] = content
@@ -132,7 +132,7 @@ def get_feed(url: str):
     if "status" in feed_content and feed_content["status"] == 200:
         return feed_content
     else:
-        print(f"Couldn't get feed {url}")
+        print(f"[!] Couldn't get feed {url}")
         return None
 
 def init_feedurls_db(urls, db_data):
@@ -153,23 +153,24 @@ def leech_new_entries(feed_data, db_data):
             continue
         last_leech_db = db_data[u][ATTR_LAST_LEECH]
         last_leech_feed = feed_data[u].entries[0]["id"]
-        print(f"feed {u}, comparing db last leech {last_leech_db} with feed last leech {last_leech_feed}")
+        print(f"[*] feed {u}, comparing db last leech {last_leech_db} with feed last leech {last_leech_feed}")
         if last_leech_db != last_leech_feed:
-            print("new shit found into feed")
+            print("[+] new shit found into feed")
             is_new_entries = True
             new_entries = get_new_entries(feed_data, u, last_leech_db)
             # leech new entries
             for n in new_entries:
+                print("~LEECH TIME~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
                 leech_res, leeched_file = leech_entry(u, n)
                 if leech_res:
                     update_entry_leech(feed_data, db_data, u, n, leeched_file)
                 else:
-                    print(f"FAILED leeching {n['link']}")
+                    print(f"[!] FAILED leeching {n['link']}")
                     leech_new_entries_res = False
                     break
             update_last_leech(feed_data, db_data, u, last_leech_feed)
         else:
-            print("nothing new found into feed")
+            print("[+] nothing new found into feed")
     return leech_new_entries_res
 
 def get_new_entries(feed_data, feed_url, last_leech_db):
@@ -179,7 +180,7 @@ def get_new_entries(feed_data, feed_url, last_leech_db):
     for e in feed_data[feed_url].entries:
         if e["id"] == last_leech_db:
             #print(f"last leech found: {e}")
-            print(f"last leech found into new feed {last_leech_db}")
+            print(f"[+] last leech found into new feed {last_leech_db}")
             break
         else:
             entries_to_leech.append(e)
@@ -191,7 +192,7 @@ def leech_entry(url, entry):
     leeched_file = None
     link = entry["link"]
     article_pattern = re.compile(r"/[\w\-]+/?")
-    print(f"leeching {entry['title']} {link}")
+    print(f"[*] leeching {entry['title']} from {link}")
     if ("youtube.com" in link or
         "youtu.be" in link):
         leech_res, leeched_file = leech_entry_yt(link)
@@ -205,12 +206,13 @@ def leech_entry(url, entry):
          article_pattern.search(link)):
         leech_res, leeched_file = leech_entry_article(link, entry["id"])
     else:
-        print(f"no extractor found for {link}")
+        print(f"[!] no extractor found for {link}")
     return leech_res, leeched_file
 
 # Return (result, filename)
 def leech_entry_yt(url):
     #print(f"DEBUG: {LEECH_DIR}")
+    print("[+] YOUTUBE extractor chosen")
 
     yt_dlp_res = True
     yt_paths = {}
@@ -245,7 +247,7 @@ def leech_entry_yt(url):
         except yt_dlp.utils.YoutubeDLError as e:
             #print(f"yt_dlp error {e}")
             if "Video unavailable" in str(e):
-                print(f"Can't leech {url}: video unavailable: deleted / geo-fencing / ... => LEECH IGNORED")
+                print(f"[!] Can't leech {url}: video unavailable: deleted / geo-fencing / ... => LEECH IGNORED")
                 output_filename = "UNAVAILABLE"
             else:
                 yt_dlp_res = False
@@ -256,6 +258,7 @@ def leech_entry_yt(url):
 
 # Return (result, filename)
 def leech_entry_ddl(url):
+    print("[+] DDL extractor chosen")
     leech_res = True
     output_filename = None
     headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -278,6 +281,7 @@ def leech_entry_ddl(url):
 
 # Return (result, filename)
 def leech_entry_article(url, entry_id):
+    print("[+] ARTICLE (HTML TO PDF) extractor chosen")
     article_res = True
     output_filename = None
     article_data = []
@@ -291,20 +295,20 @@ def leech_entry_article(url, entry_id):
     try:
         HTML(url).write_pdf(output_fullpath)
     except Exception as e:
-        print(f"Error processing {base_url}: {str(e)}")
+        print(f"[!] Error processing {base_url}: {str(e)}")
         article_res = False
 
     return article_res, output_filename
 
 # update id of last leech
 def update_last_leech(feed_data, db_data, url, entry_id):
-    print(f"last entry for {url}: {entry_id}")
+    print(f"[+] last entry for {url}: {entry_id}")
     db_data[url][ATTR_LAST_LEECH] = entry_id
 
 # trace id with filename
 def update_entry_leech(feed_data, db_data, url, entry, filename):
     entry_id = entry["id"]
-    print(f"entry {entry_id}: file {filename}")
+    print(f"[+] entry {entry_id} saved to file {filename}")
     db_data[url][entry_id] = filename
 
 def db_update(db_data):
@@ -312,7 +316,7 @@ def db_update(db_data):
     with open(DB_FILE_NAME, "wb") as f:
         tomli_w.dump(db_data, f)
     f.close()
-    print("db saved")
+    print("[+] db saved")
 
 if __name__ == "__main__":
     main()
